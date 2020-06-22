fb16dcf25dc61196a4fb0213fe6f50761c17fa3f
Morel <morelgarisi5@gmail.com>
2020-06-22 10:55:45 +0300

    Update index.js

--- src/components/step/index.js
+++ src/components/step/index.js
@@ -55,7 +55,7 @@ export class Step extends React.Component<StepProps> {
     const safePosition = getSafePercent(position);
 
     let style = {
-      left: `${safePosition}%`,
+      right: `${safePosition}%`,
       transitionDuration: `${transitionDuration}ms`,
     };
 
